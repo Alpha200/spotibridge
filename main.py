4c95914eb9fa811b5e5fc08a2ebe6c4dbcc30445
Daniel Sendzik <daniel@sendzik.eu>
2019-08-16 00:07:26 +0200

    First working version

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+from colorsys import hsv_to_rgb
+from datetime import datetime, timedelta
+from random import random
+
+from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+import paho.mqtt.client as mqtt
 from spotipy import util, Spotify
 
 client_id = '***REMOVED***'
 client_secret = '***REMOVED***'
 redirect_uri = 'http://localhost:17382/redirect'
+mqtt_host = "192.168.178.206"
 
 
 def format_seconds(seconds):
@@ -10,6 +18,79 @@ def format_seconds(seconds):
     num_seconds = round(seconds % 60)
     milliseconds = int((seconds - int(seconds)) * 1000)
     return "{:02}:{:02}:{:03}".format(minutes, num_seconds, milliseconds)
+
+
+class ColorScheduler:
+    def __init__(self, token):
+        self.sp = Spotify(auth=token)
+        self.scheduler = BlockingScheduler()
+        self.analysis = None
+        self.mqttc = mqtt.Client()
+
+        self.mqttc.connect(mqtt_host)
+        self.mqttc.loop_start()
+
+        def update_job_caller():
+            self.update_job()
+
+        self.scheduler.add_job(
+            update_job_caller,
+            'interval',
+            (),
+            id='job_updater',
+            seconds=5
+        )
+
+    def start(self):
+        self.scheduler.start()
+
+    def update_job(self):
+        def update_color_caller():
+            self.update_color()
+
+        current_track = self.sp.current_user_playing_track()
+
+        now = datetime.now()
+
+        if current_track is None or not current_track['is_playing']:
+            job = self.scheduler.get_job('color_updater')
+
+            if job is not None:
+                job.remove()
+
+            print("No track playing")
+
+            return
+
+        track_id = current_track['item']['id']
+
+        if self.analysis is None or self.analysis[1] != track_id:
+            self.analysis = (self.sp.audio_analysis(track_id), track_id)
+
+        start_of_track = now - timedelta(milliseconds=current_track['progress_ms'])
+        progress_in_seconds = current_track['progress_ms'] / 1000
+
+        sections = self.analysis[0]['sections']
+
+        next_section = next((section for section in sections if progress_in_seconds < section['start']), None)
+
+        if next_section is not None:
+            print("Updating job!")
+
+            next_change = start_of_track + timedelta(seconds=next_section['start'])
+            self.scheduler.add_job(
+                update_color_caller,
+                'date',
+                (),
+                id="color_updater",
+                run_date=next_change,
+                replace_existing=True
+            )
+
+    def update_color(self):
+        r, g, b = tuple(int(x * 255) for x in hsv_to_rgb(random(), 1.0, 1.0))
+        color = "{},{},{}".format(r, g, b)
+        self.mqttc.publish("homie/ledring/ring/color/set", color, qos=1)
 
 
 def main():
@@ -21,31 +102,8 @@ def main():
         redirect_uri=redirect_uri
     )
 
-    sp = Spotify(auth=token)
-
-    current_track = sp.current_user_playing_track()
-
-    if current_track is None:
-        print("No track is being played!")
-        return
-
-    print("Track: {}".format(current_track['item']['name']))
-
-    track_id = current_track['item']['id']
-
-    analysis = sp.audio_analysis(track_id)
-
-    print("Duration: {}".format(format_seconds(analysis['track']['duration'])))
-    print("Fade In: {}".format(format_seconds(analysis['track']['end_of_fade_in'])))
-    print("Fade Out: {}".format(format_seconds(analysis['track']['start_of_fade_out'])))
-    print()
-
-    for section in analysis['sections']:
-        print("Start: {}".format(format_seconds(section['start'])))
-        print("Confidence: {}".format(section['confidence']))
-        print("Loudness: {}".format(section['loudness']))
-        print("Tempo: {}".format(section['tempo']))
-        print()
+    color_scheduler = ColorScheduler(token)
+    color_scheduler.start()
 
 
 if __name__ == '__main__':
