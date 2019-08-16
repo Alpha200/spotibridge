1b41d2b4d241a244ea12a145938b5eb4f811cd50
Daniel Sendzik <daniel@sendzik.eu>
2019-08-17 00:27:37 +0200

    New color calculation

---
+++ colorfinder.py
@@ -0,0 +1,71 @@
+def color_filter_hue(r, g, b):
+    return (abs(r - g) * abs(r - g) + abs(r - b) *abs(r - b) + abs(g - b) * abs(g - b)) / 65535 * 50 + 1
+
+
+class ColorFinder:
+    def __init__(self, color_filter):
+        self.color_filter = color_filter
+
+    def get_most_prominent_color(self, image):
+        rgb = None
+        image_data = self.get_image_data(image)
+
+        for degrade in (6, 4, 2, 0):
+            rgb = self.get_most_prominent_rgb(image_data, degrade, rgb)
+
+        return rgb['r'], rgb['g'], rgb['b']
+
+    @staticmethod
+    def does_rgb_match(rgb, pixel):
+        if rgb is None:
+            return True
+
+        r = pixel['r'] >> rgb['degrade']
+        g = pixel['g'] >> rgb['degrade']
+        b = pixel['b'] >> rgb['degrade']
+
+        return r == rgb['r'] and g == rgb['g'] and b == rgb['b']
+
+    def get_most_prominent_rgb(self, pixels, degrade, rgb_match):
+        result = dict(r=0, g=0, b=0, count=0, degrade=degrade)
+        db = dict()
+
+        for pixel in pixels.values():
+            total_weight = pixel['weight'] * pixel['count']
+
+            if self.does_rgb_match(rgb_match, pixel):
+                pixel_group_key = "{},{},{}".format(pixel['r'] >> degrade, pixel['g'] >> degrade, pixel['b'] >> degrade)
+
+                if pixel_group_key not in db:
+                    db[pixel_group_key] = total_weight
+                else:
+                    db[pixel_group_key] += total_weight
+
+        for k, v in db.items():
+            if v > result['count']:
+                r, g, b = tuple(int(value) for value in k.split(','))
+                result = dict(r=r, g=g, b=b, count=v, degrade=degrade)
+
+        return result
+
+    def get_image_data(self, image):
+        result = dict()
+        length = image.width * image.height
+        factor = max(1, round(length / 1250.0))
+
+        for idx in range(0, length, factor):
+            r, g, b = image.getpixel((int(idx / image.width), idx % image.width))
+
+            key = '{},{},{}'.format(r, g, b)
+
+            if key not in result:
+                entry = dict(r=r, g=g, b=b, count=1, weight=self.color_filter(r, g, b))
+
+                if entry['weight'] <= 0:
+                    entry['weight'] = 1e-10
+
+                result[key] = entry
+            else:
+                result[key]['count'] += 1
+
+        return result
--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from colorsys import hsv_to_rgb
+import math
+from colorsys import hsv_to_rgb, rgb_to_hsv
 from datetime import datetime, timedelta
+from io import BytesIO
 from random import random
 
+import requests
+from PIL import Image
 from apscheduler.schedulers.blocking import BlockingScheduler
 import paho.mqtt.client as mqtt
 from spotipy import util, Spotify
+
+from colorfinder import ColorFinder, color_filter_hue
 
 username = '***REMOVED***'
 client_id = '***REMOVED***'
@@ -23,12 +29,14 @@ def format_seconds(seconds):
 class ColorScheduler:
     def __init__(self):
         self.scheduler = BlockingScheduler()
+        self.color_finder = ColorFinder(color_filter_hue)
         self.analysis = None
         self.mqttc = mqtt.Client()
+        self.current_hue = 0
+        self.current_s = 0
 
         self.mqttc.connect(mqtt_host)
         self.mqttc.loop_start()
-        self.previous_hue = -1
 
         def update_job_caller():
             self.update_job()
@@ -45,8 +53,8 @@ class ColorScheduler:
         self.scheduler.start()
 
     def update_job(self):
-        def update_color_caller():
-            self.update_color()
+        def update_color_caller(section):
+            self.update_color(section)
 
         token = util.prompt_for_user_token(
             username,
@@ -59,15 +67,9 @@ class ColorScheduler:
         sp = Spotify(auth=token)
 
         before = datetime.now()
-
         current_track = sp.current_user_playing_track()
-
         now = datetime.now()
-
         difference = (now - before) / 2
-
-        print("Difference: {}".format(difference))
-
         now = before + difference
 
         if current_track is None or not current_track['is_playing']:
@@ -86,6 +88,23 @@ class ColorScheduler:
         if self.analysis is None or self.analysis[1] != track_id:
             self.analysis = (sp.audio_analysis(track_id), track_id)
 
+            cover_urls = current_track['item']['album']['images']
+            cover_url = cover_urls[len(cover_urls) - 1]['url']
+
+            response = requests.get(cover_url)
+            response.raise_for_status()
+            image = Image.open(BytesIO(response.content))
+
+            r, g, b = self.color_finder.get_most_prominent_color(image)
+            h, s, v = rgb_to_hsv(r / 255.0, g / 255.0, b / 255.0)
+
+            self.current_hue = h
+            self.current_s = s
+
+            print("H: {}, S: {}, V: {}".format(h, s, v))
+
+            self.update_color(self.analysis[0]['sections'][0])
+
         start_of_track = now - timedelta(milliseconds=current_track['progress_ms'])
         progress_in_seconds = current_track['progress_ms'] / 1000
 
@@ -103,22 +122,21 @@ class ColorScheduler:
         self.scheduler.add_job(
             update_color_caller,
             'date',
-            (),
+            (next_section,),
             id="color_updater",
             run_date=next_change,
             replace_existing=True
         )
 
-    def update_color(self):
-        new_hue = random()
+    def update_color(self, section):
+        if section is not None:
+            brightness = max(0.1, min(1.0, (15.0 - (section['loudness'] * -1 - 5.0)) / 15))
+        else:
+            brightness = 0.1
 
-        if self.previous_hue != -1:
-            while abs(self.previous_hue - new_hue) < 0.2 or abs((1 - self.previous_hue) - new_hue) < 0.2:
-                new_hue = random()
+        print("New brightness: {}".format(brightness))
+        r, g, b = tuple(int(x * 255) for x in hsv_to_rgb(self.current_hue, math.sqrt(self.current_s), brightness))
 
-        self.previous_hue = new_hue
-
-        r, g, b = tuple(int(x * 255) for x in hsv_to_rgb(new_hue, 1.0, 1.0))
         color = "{},{},{}".format(r, g, b)
         self.mqttc.publish("homie/ledring/ring/color/set", color, qos=1)
 
